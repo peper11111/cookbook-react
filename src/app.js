9ed2a4211d7d469a6d7606590e8ee993f8e8ba1e
Piotr Pleban <peper.pleban@gmail.com>
2018-10-25 11:56:02 +0200

    Added react-router to project.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Router } from 'react-router-dom'
+import history from '@/router/history'
+import routes from '@/router/routes'
 
 class App extends React.Component {
   render () {
-    return <div>App</div>
+    return (
+      <Router history={ history }>
+        <React.Fragment>
+          { routes.map((route) => (
+            <Route
+              exact
+              key={ route.path }
+              path={ route.path }
+              component={ route.component }
+            />
+          )) }
+        </React.Fragment>
+      </Router>
+    )
   }
 }
 
