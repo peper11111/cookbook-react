5aa28db4f90268fdc0e34ca76575eb2bb22ee328
Piotr Pleban <peper.pleban@gmail.com>
2018-10-25 11:37:11 +0200

    Changed app component file name. Removed initial code.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'izitoast/dist/css/iziToast.css'
 import '@/assets/styles/index.scss'
-import App from './App'
+import App from '@/app'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
