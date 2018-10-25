25aa80a77a591d29bcbb2210bfe5914cb02d5172
Piotr Pleban <peper.pleban@gmail.com>
2018-10-25 11:16:56 +0200

    Added global page-styles.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'izitoast/dist/css/iziToast.css'
-import './index.css'
+import '@/assets/styles/index.scss'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
