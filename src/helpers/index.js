8233b44e54c75d44f05d179670ef538e0eaf8716
Piotr Pleban <peper.pleban@gmail.com>
2018-11-02 13:17:24 +0100

    Fixed requiresAuth method causing recursion.

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,7 +3,10 @@ import routes from '@/router/routes'
 
 export default {
   requiresAuth (pathname) {
-    const matched = routes.find((route) => matchPath(pathname, route))
+    const matched = routes.find((route) => {
+      const match = matchPath(pathname, route)
+      return match && match.exact
+    })
     return matched && matched.meta && matched.meta.requiresAuth
   }
 }
