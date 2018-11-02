5dcc31aa4494aa885214f1e46e41d595d5cb4042
Piotr Pleban <peper.pleban@gmail.com>
2018-11-02 14:01:20 +0100

    Created simple sign-in page with one input.

--- src/pages/intro/sign-in-page.js
+++ src/pages/intro/sign-in-page.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import i18n from '@/i18n'
 
 class SignInPage extends React.Component {
   render () {
-    return <div>SignInPage</div>
+    return (
+      <div className="o-page o-page--intro">
+        <div className="o-card">
+          <h1 className="o-card__header">
+            { i18n.t('global.app') }
+          </h1>
+          <div className="o-page__separator o-page__separator--intro"></div>
+          <form className="o-form">
+            <input
+              placeholder={ i18n.t('form.login') }
+              className="o-form__input o-form__input--full"
+              type="text"
+            />
+          </form>
+        </div>
+      </div>
+    )
   }
 }
 
