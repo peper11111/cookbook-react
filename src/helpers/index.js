b363fa361ba13aaee224222cb70b71ff845b6663
Piotr Pleban <peper.pleban@gmail.com>
2018-11-02 15:48:46 +0100

    Added missing global methods to helpers.

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,9 +1,12 @@
 import { matchPath } from 'react-router-dom'
 import queryString from 'query-string'
+import api from '@/api'
 import helpers from '@/helpers'
+import i18n from '@/i18n'
 import history from '@/router/history'
 import routes from '@/router/routes'
 import store from '@/store'
+import { signIn, setCategories, setCuisines } from '@/store/actions'
 
 export default {
   checkNavigation () {
@@ -24,6 +27,43 @@ export default {
     return !!(matched && matched.meta && matched.meta.requiresAuth)
   },
   fetchGlobalData () {
-    return Promise.resolve()
+    return helpers.fetchCurrentUser().then(() => {
+      return helpers.fetchCategories()
+    }).then(() => {
+      return helpers.fetchCuisines()
+    })
+  },
+  fetchCurrentUser () {
+    return api.users.current().then((value) => {
+      store.dispatch(signIn(value.data))
+    })
+  },
+  fetchCategories () {
+    return api.categories.readAll().then((value) => {
+      const categories = helpers.mapAndSortCategories(value.data)
+      store.dispatch(setCategories(categories))
+    })
+  },
+  mapAndSortCategories (categories) {
+    return categories.map((category) => {
+      category.name = i18n.t(`recipe.category.${category.name}`)
+      return category
+    }).sort((a, b) => {
+      return a.name.localeCompare(b.name)
+    })
+  },
+  fetchCuisines () {
+    return api.cuisines.readAll().then((value) => {
+      const cuisines = helpers.mapAndSortCuisines(value.data)
+      store.dispatch(setCuisines(cuisines))
+    })
+  },
+  mapAndSortCuisines (cuisines) {
+    return cuisines.map((cuisine) => {
+      cuisine.name = i18n.t(`recipe.cuisine.${cuisine.name}`)
+      return cuisine
+    }).sort((a, b) => {
+      return a.name.localeCompare(b.name)
+    })
   }
 }
