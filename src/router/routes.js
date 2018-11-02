5de211d358f193275df018a73039c9d25fbbd89b
Piotr Pleban <peper.pleban@gmail.com>
2018-11-02 12:50:00 +0100

    Added simple sign-in page.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,14 @@
 import HomePage from '@/pages/home-page'
+import SignInPage from '@/pages/intro/sign-in-page'
 
 export default [
   {
     path: '/',
     component: HomePage,
     meta: { requiresAuth: true }
+  },
+  {
+    path: '/sign-in',
+    component: SignInPage
   }
 ]
