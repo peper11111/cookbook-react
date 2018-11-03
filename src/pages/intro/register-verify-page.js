41804395cb3e965c86f856534e4c6ba1795e16df
Piotr Pleban <peper.pleban@gmail.com>
2018-11-03 12:11:34 +0100

    Finished registerVerifyPage component.

--- src/pages/intro/register-verify-page.js
+++ src/pages/intro/register-verify-page.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+import queryString from 'query-string'
+import requester from '@/hoc/requester'
 
 class RegisterVerifyPage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      pending: this.props.pending
+    }
+    this.wrap = this.props.wrap.bind(this)
+  }
+  componentDidMount () {
+    this.wrap(this.registerVerify())
+  }
+  registerVerify () {
+    const query = queryString.parse(this.props.history.location.search)
+    return this.$api.auth.registerVerify({
+      uuid: query.uuid
+    }).then(() => {
+      this.$notify.success('user-verified')
+    }).finally(() => {
+      this.props.history.push('/sign-in')
+    })
+  }
   render () {
-    return <div>RegisterVerifyPage</div>
+    return (
+      <div className="o-page"/>
+    )
   }
 }
 
-export default RegisterVerifyPage
+export default requester(withRouter(RegisterVerifyPage))
