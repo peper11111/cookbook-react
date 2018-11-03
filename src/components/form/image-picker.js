7a21233318e04d1cf4ae9d9be4ac1961d6af7c60
Piotr Pleban <peper.pleban@gmail.com>
2018-11-03 23:18:58 +0100

    Created simple ImagePicker component.

---
+++ src/components/form/image-picker.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '@/components/form/image-picker.scss'
+
+class ImagePicker extends React.Component {
+  get bannerSrc () {
+    return this.$helpers.imageSrc(this.props.value) || this.props.blank
+  }
+  render () {
+    return (
+      <div className={ `c-image-picker ${this.props.className}`}>
+        <img
+          alt=""
+          className="c-image-picker__image"
+          src={ this.bannerSrc }
+        />
+      </div>
+    )
+  }
+}
+
+export default ImagePicker
--- src/components/user/user-details.js
+++ src/components/user/user-details.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import ImagePicker from '@/components/form/image-picker'
 import '@/components/user/user-details.scss'
 
 class UserDetails extends React.Component {
   render () {
     return (
-      <div>UserDetails</div>
+      <div className="c-user-details">
+        <ImagePicker
+          className="c-user-details__banner"
+          value={ this.props.user.bannerId }
+        />
+      </div>
     )
   }
 }
 
-export default UserDetails
+const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(UserDetails)
