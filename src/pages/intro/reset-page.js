83d838468fc4c72485e996ff6552335687283238
Piotr Pleban <peper.pleban@gmail.com>
2018-11-03 12:28:05 +0100

    Finished resetPage component.

--- src/pages/intro/reset-page.js
+++ src/pages/intro/reset-page.js
@@ -1,9 +1,89 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import form from '@/hoc/form'
+import requester from '@/hoc/requester'
 
 class ResetPage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      login: this.props.login,
+      pending: this.props.pending
+    }
+    this.wrap = this.props.wrap.bind(this)
+  }
+  reset () {
+    return this.$api.auth.reset({
+      login: this.state.login
+    }).then(() => {
+      this.setState({ done: true })
+    })
+  }
+  resetResend () {
+    return this.$api.auth.resetResend({
+      login: this.state.login
+    }).then(() => {
+      this.$notify.info('message-resend')
+    })
+  }
   render () {
-    return <div>ResetPage</div>
+    return (
+      <div className="o-page o-page--intro">
+        <div className="o-card">
+          <h1 className="o-card__header">
+            { this.$i18n.t('global.app') }
+          </h1>
+          <div className="o-page__separator o-page__separator--intro"/>
+          { !this.state.done ? (
+            <form
+              onSubmit={ (event) => {
+                event.preventDefault()
+                this.wrap(this.reset())
+              } }
+              className="o-form"
+            >
+              <input
+                className="o-form__input o-form__input--full"
+                onChange={ (event) => this.setState({ login: event.target.value })}
+                placeholder={ this.$i18n.t('form.login') }
+                type="text"
+                value={ this.state.login }
+              />
+              <input
+                className={ `o-button o-button__accent o-button--full ${this.state.pending ? 'is-disabled' : ''}`}
+                type="submit"
+                value={ this.$i18n.t('form.reset-password') }
+              />
+              <p className="o-form__footer">
+                { this.$i18n.t('form.remember-password') }
+                <Link
+                  className="o-form__action"
+                  to="/sign-in"
+                >
+                  { this.$i18n.t('form.sign-in') }
+                </Link>
+              </p>
+            </form>
+          ) : (
+            <div>
+              <p className="o-card__message">
+                <i className="o-card__check material-icons">
+                  check_circle_outline
+                </i>
+                { this.$i18n.t('form.password-reset-email-sent') }
+              </p>
+              <div
+                className={ `o-button o-button__accent o-button--full ${this.state.pending ? 'is-disabled' : ''}` }
+                onClick={ () => this.wrap(this.resetResend()) }
+              >
+                { this.$i18n.t('form.resend-email') }
+              </div>
+            </div>
+          ) }
+        </div>
+      </div>
+    )
   }
 }
 
-export default ResetPage
+export default form(requester(ResetPage))
