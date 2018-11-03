74406cb3c4025057daa3213b1ebfc40e96937dbe
Piotr Pleban <peper.pleban@gmail.com>
2018-11-03 20:39:52 +0100

    Added wrapper to UserPage.

--- src/pages/user-page.js
+++ src/pages/user-page.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import requester from '@/hoc/requester'
 
 class UserPage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      pending: this.props.pending
+    }
+    this.wrap = this.props.wrap.bind(this)
+  }
   render () {
-    return <div>UserPage</div>
+    if (!this.state.pending) {
+      return (
+        <div className="o-page">
+          <div className="o-page__wrapper o-page__wrapper--top">
+          </div>
+        </div>
+      )
+    } else {
+      return (null)
+    }
   }
 }
 
-export default UserPage
+export default requester(UserPage)
