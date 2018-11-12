f01ccf0e0f4865a6f7fe6c7d3ff3696ef9715752
Piotr Pleban <peper.pleban@gmail.com>
2018-11-12 21:39:21 +0100

    Finished ExplorePage component.

--- src/pages/explore-page.js
+++ src/pages/explore-page.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import RecipeFilters from '@/components/list/recipe-filters'
+import RecipeList from '@/components/list/recipe-list'
 
 class ExplorePage extends React.Component {
   render () {
-    return <div>ExplorePage</div>
+    return (
+      <div className="o-page">
+        <div className="o-page__wrapper o-page__wrapper--list">
+          <div className="o-page__sidebar">
+            <RecipeFilters/>
+          </div>
+          <div className="o-page__container">
+            <RecipeList type="recipes"/>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
