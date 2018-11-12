0841da9e5001a83368b4a76926370dd04d7cd316
Piotr Pleban <peper.pleban@gmail.com>
2018-11-12 21:41:13 +0100

    Finished FavouritePage component.

--- src/pages/favourite-page.js
+++ src/pages/favourite-page.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import RecipeFilters from '@/components/list/recipe-filters'
+import RecipeList from '@/components/list/recipe-list'
 
 class FavouritePage extends React.Component {
   render () {
-    return <div>FavouritePage</div>
+    return (
+      <div className="o-page">
+        <div className="o-page__wrapper o-page__wrapper--list">
+          <div className="o-page__sidebar">
+            <RecipeFilters/>
+          </div>
+          <div className="o-page__container">
+            <RecipeList
+              userId={ this.props.authUser.id }
+              type="user-favourites"
+            />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-export default FavouritePage
+const mapStateToProps = (state) => ({
+  authUser: state.auth.user
+})
+
+export default connect(mapStateToProps)(FavouritePage)
