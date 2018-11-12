8865e35356894e3f5af796b640848e91b8625c68
Piotr Pleban <peper.pleban@gmail.com>
2018-11-12 18:39:14 +0100

    Created form-select component.

---
+++ src/components/form/form-select.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+class FormSelect extends React.Component {
+  label () {
+    return this.props.options.find((option) => {
+      return this.isSelected(option)
+    }).label
+  }
+  isSelected (option) {
+    return String(option.value) === String(this.props.value)
+  }
+  onChange (event) {
+    this.props.onChange(event.target.value)
+  }
+  render () {
+    if (this.props.disabled) {
+      return (
+        <div>
+          { this.label() }
+        </div>
+      )
+    } else {
+      return (
+        <select
+          onChange={ (event) => this.onChange(event) }
+          className="o-form__select"
+          value={ this.props.value || '' }
+        >
+          <option value=""/>
+          { this.props.options.map((option) => (
+            <option
+              key={ option.value }
+              value={ option.value }
+            >
+              { option.label }
+            </option>
+          )) }
+        </select>
+      )
+    }
+  }
+}
+
+export default FormSelect
