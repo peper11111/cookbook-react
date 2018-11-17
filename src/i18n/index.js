fb0acecb83f44f44bcabd273176095decddf76a2
Piotr Pleban <peper.pleban@gmail.com>
2018-11-17 14:13:53 +0100

    Fixed moment locales.

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,6 +2,7 @@ import i18n from 'i18next'
 import moment from 'moment'
 import config from '@/config'
 import pl from '@/i18n/translation/pl.json'
+import 'moment/locale/pl'
 
 moment.locale(config.locale)
 
